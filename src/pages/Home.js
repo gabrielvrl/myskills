e57333b1f6ba5c1719cc621c28eca72e69254e76
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2022-07-11 20:41:05 -0300

    Estrutura JSX

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export function Home() {
+  return(
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <Text>React Native</Text>
+    </View>
+  )
+}
